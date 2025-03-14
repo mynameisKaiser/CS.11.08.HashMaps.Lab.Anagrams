c7ef7a9338ea331ad23b6dcd3f787e2e8e4c1cce
mynameisKaiser <kaisertsoi@hotmail.com>
2025-03-14 14:44:41 +0800

    Commit (mass commit)

--- src/main/java/AnagramSolver.java
+++ src/main/java/AnagramSolver.java
@@ -1,6 +1,9 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
-
+import java.util.List;
 public class AnagramSolver {
 
     private AnagramSolver() {};
@@ -12,9 +15,19 @@ public class AnagramSolver {
      * @return
      */
     public static HashMap<String, ArrayList<String>> anagrams(String filename) {
-        return null;
+        HashMap<String, ArrayList<String>> hashmap = new HashMap<>();
+        try (BufferedReader reader = new BufferedReader(new FileReader(filename))) {
+            String word;
+            while ((word = reader.readLine()) != null) {
+                String sortedWord = sortString(word);
+                hashmap.putIfAbsent(sortedWord, new ArrayList<>());
+                hashmap.get(sortedWord).add(word);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return hashmap;
     }
-
     /**
      * Input: hashmap of lists of words that are anagrams.
      * Output: largest list of words in hashmap that are anagrams.
@@ -22,7 +35,16 @@ public class AnagramSolver {
      * @return
      */
     public static ArrayList<String> mostFrequentAnagram(HashMap<String, ArrayList<String>> anagrams) {
-        return null;
+        int Max  = 0;
+        ArrayList<String> listOfFrequent = new ArrayList<>();
+
+        for (ArrayList<String> anagramList : anagrams.values()) {
+            if (anagramList.size() > Max) {
+                Max = anagramList.size();
+                listOfFrequent = anagramList;
+            }
+        }
+        return listOfFrequent;
     }
 
     /**
@@ -31,7 +53,13 @@ public class AnagramSolver {
      * @param anagrams
      */
     public static void printKeyValuePairs(HashMap<String, ArrayList<String>> anagrams) {
-
+        for (String key : anagrams.keySet()) {
+            System.out.println(key + ": " + anagrams.get(key));
+        }
     }
-
+    private static String sortString(String string) {
+        char[] charArray = string.toCharArray();
+        java.util.Arrays.sort(charArray);
+        return new String(charArray);
+    }
 }
